1a372357a37f5f4cd9ac815c75b6e344fcdd0313
AlIbay <43456952+AlIbay8@users.noreply.github.com>
2024-04-13 09:12:15 -0700

    display table

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let root = document.getElementById("root")
 let textArea = document.createElement("textarea")
 let submitBtn = document.createElement("button")
-submitBtn.textContent = "Submit"
+submitBtn.textContent="Submit"
+let table = document.createElement("table")
 
 root.appendChild(textArea)
 root.appendChild(submitBtn)
+root.appendChild(table)
 
 submitBtn.addEventListener("click", processInput)
-
-
 
 function processInput() {
     let wordFrequency = countWords()
@@ -28,6 +28,7 @@ function countWords() {
             wordCounts[word] = 1
         }
     }
+    console.log(wordCounts)
     return wordCounts
 }
 
@@ -43,9 +44,31 @@ function sortWords(wordCounts = {}) {
 }
 
 function displayTopWords(sortedKeys = [], wordCounts = {}) {
+    table.textContent = ""
+    
+    let headerRow = table.insertRow()
+    let wordHeader = document.createElement("th")
+    let countHeader = document.createElement("th")
+    
+    wordHeader.textContent = "word_name"
+    countHeader.textContent = "word_frequency"
+
+    headerRow.appendChild(wordHeader)
+    headerRow.appendChild(countHeader)
+    
     for (let i=0; i<5; i++) {
         if (i<sortedKeys.length) {
+            let word = document.createElement("td")
+            let count = document.createElement("td")
             console.log(`${sortedKeys[i]}: ${wordCounts[sortedKeys[i]]}`)
+            let row = table.insertRow()
+            
+            word.textContent = `${sortedKeys[i]}`
+            row.appendChild(word)
+
+            count.textContent = `${wordCounts[sortedKeys[i]]}`
+            row.appendChild(count)
+
         }
     }
 }
